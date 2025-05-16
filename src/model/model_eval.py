6cf12cbf1aa6716f60d657ec7475baeaf00368b7
kabbstat <statkabb95@gmail.com>
2025-05-17 00:41:46 +0100

    the best experiment hyper

--- src/model/model_eval.py
+++ src/model/model_eval.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 import json
 import pickle
+from dvclive import Live
+import yaml
 
 def load_data(filepath):
     try:
@@ -27,10 +29,20 @@ def predict_model(X):
 #y_pred = model.predict(X_test)
 def evaluation(y_test, y_pred):
     try :
+        params = yaml.safe_load(open('params.yaml', 'r'))
+        test_size = params["data_collection"]["test_size"]
+        n_estimators = params["model_building"]["n_estimators"]
         acc = accuracy_score(y_test, y_pred)
         pre = precision_score(y_test, y_pred)
         re = recall_score(y_test, y_pred)
         f1 = f1_score(y_test, y_pred)
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', acc)
+            live.log_metric('precision', pre)
+            live.log_metric('recall', re)
+            live.log_metric('f1_score', f1)
+            live.log_param('test_size', test_size)
+            live.log_param('n_estimators', n_estimators)
         metrics ={
             'accuracy': acc,
             'precision': pre,
